477223ae473d59748832008b7915a1d75e76c0e8
TSuleymani <tural.suleymani@xpressdox.com>
2021-07-31 19:54:04 +0400

    fixed swagger issue

--- TradingAPI/Startup.cs
+++ TradingAPI/Startup.cs
@@ -44,8 +44,6 @@ namespace TradingAPI
 
             app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
